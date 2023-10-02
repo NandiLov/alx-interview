40e0e1e38a957c967fd7c19a3e809de0a3221751
NandiLov <nandilamul@gmail.com>
2023-10-02 01:40:47 -0700

    edit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,37 +3,24 @@
 '''
 
 def pascal_triangle(n):
-    # Check if n is less than or equal to 0
     if n <= 0:
         return []
 
-    # Initialize the triangle list
     triangle = []
-
-    # Generate each row
     for i in range(n):
-        # Initialize the row list for this iteration
-        row = []
-
-        # Calculate the values in the row
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                # The first and last values in each row are always 1
-                row.append(1)
-            else:
-                # Other values are the sum of the two values above them
-                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
-                row.append(value)
-
-        # Append the row to the triangle list
+        row = [1]  # The first element in each row is always 1
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)  # The last element in each row is always 1
         triangle.append(row)
 
-    # Return the Pascal's Triangle
     return triangle
 
-#calling the above function with an integer n as an argument to generate Pascal's Triangle up to the nth row.
-# print result
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
-result = pascal_triangle(5)
-for row in result:
-    print(row)
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
